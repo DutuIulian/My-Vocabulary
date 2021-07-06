d19d26bf1cce0b2b45dec5eaad202f541d798e72
Dutu Iulian <dutu.iulian98@gmail.com>
2021-07-06 19:39:08 +0300

    Fixed bug: if the database changed, then the correct answer must not be shown for the current definition

--- app/src/main/java/com/example/belvito/japanesevocabulary/HomeFragment.java
+++ app/src/main/java/com/example/belvito/japanesevocabulary/HomeFragment.java
@@ -38,6 +38,7 @@ public class HomeFragment extends Fragment {
         if(databaseChanged) {
             definitionsManager.repopulateList();
             currentDefinition = definitionsManager.getNextDefinition(true);
+            currentState = State.NOTHING;
             databaseChanged = false;
         }
         if(currentDefinition == null) {
